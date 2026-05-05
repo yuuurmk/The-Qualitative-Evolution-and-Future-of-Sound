4026f8c984878df51858fb52c568c41f0cb4c503
yuuurmk <torng93610@gmail.com>
2026-05-06 02:38:54 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/The-Qualitative-Evolution-and-Future-of-Sound/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
